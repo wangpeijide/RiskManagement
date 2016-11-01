722c9338189f47806e97377c24df946b4bb831cb
XIA JING <131250162@smail.nju.edu.cn>
2016-11-01 12:15:33 +0800

    add path to docker home

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cd "E:\eclipse\php\RiskManagement"
-docker kill riskmanagement
-docker rmi --force risk-management-nginx
-docker build -t risk-management-nginx .
-docker run --name riskmanagement -d -p 80:80 risk-management-nginx
+DOCKER_HOME="D:/Program Files/Docker Toolbox/"
+cd "E:/eclipse/php/RiskManagement"
+${DOCKER_HOME}docker kill riskmanagement
+${DOCKER_HOME}docker rmi --force risk-management-nginx
+${DOCKER_HOME}docker build -t risk-management-nginx .
+${DOCKER_HOME}docker run --name riskmanagement -d -p 80:80 risk-management-nginx
 
-exec "$BASH" --login -i
+#exec "$BASH" --login -i
