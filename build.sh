6230ca7bbef81981d78ec32bd1d22d4c60e4194a
XIA JING <131250162@smail.nju.edu.cn>
2016-11-01 13:24:29 +0800

    have to commit to test jenkins

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DOCKER_HOME="D:/Program Files/Docker Toolbox/"
 cd "E:/eclipse/php/RiskManagement"
-${DOCKER_HOME}docker kill riskmanagement
-${DOCKER_HOME}docker rmi --force risk-management-nginx
-${DOCKER_HOME}docker build -t risk-management-nginx .
-${DOCKER_HOME}docker run --name riskmanagement -d -p 80:80 risk-management-nginx
+"${DOCKER_HOME}docker" kill riskmanagement
+"${DOCKER_HOME}docker" rmi --force risk-management-nginx
+"${DOCKER_HOME}docker" build -t risk-management-nginx .
+"${DOCKER_HOME}docker" run --name riskmanagement -d -p 80:80 risk-management-nginx
 
 #exec "$BASH" --login -i
